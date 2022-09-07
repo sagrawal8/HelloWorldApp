64234b98706066addaabc8655982535bda5ef7b5
Shashank Agrawal <agrawal.sha@northeastern.edu>
2022-09-07 15:21:48 -0400

    Project 1

---
+++ app/src/main/java/com/example/numad22fa_shashankagrawal/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.numad22fa_shashankagrawal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void aboutMe(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "Name: Shashank Agrawal\nEmail: agrawal.sha@northeastern.edu";
+        int duration = Toast.LENGTH_LONG;
+        Toast.makeText(context, text, duration).show();
+    }
+
+}
